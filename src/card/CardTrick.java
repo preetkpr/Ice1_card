1c0c9262856251fad986d7f230142bfe6ef7a399
preetkpr <preetkpr@sheridancollege.ca>
2025-05-27 21:27:04 -0400

    card version 1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -9,18 +9,23 @@ package card;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author srinivsi
+ * Modifier: Preet Kaur
+ * Student ID: 991776800
+ * Date Modified: 2025-05-27
  */
 public class CardTrick {
     
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        Random rand = new Random();
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(rand.nextInt(13) + 1); 
+            c.setSuit(Card.SUITS[rand.nextInt(4)]); 
+            magicHand[i] = c;
         }
         
         //insert code to ask the user for Card value and suit, create their card
